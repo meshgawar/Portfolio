e7d74eaa61091bce1516ed8da5f564d5c6370501
Mesh Gawar <meshgawar@gmail.com>
2025-08-27 23:00:49 +0530

    Css updated

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -3,18 +3,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <section className="footer section" id="footer">
-            <div style={{ display: "flex", gap: "1.5rem", fontSize: "2rem" }}>
-      {/* Font Awesome Icons */}
-      <i className="fa fa-code"></i>
-      <i className="fa fa-user"></i>
-      <i className="fa fa-laptop-code"></i>
-      <i className="fa fa-rocket"></i>
-
-      {/* Boxicons Icons */}
-      <i className="bx bx-code"></i>
-      <i className="bx bxs-user"></i>
-      <i className="bx bxs-rocket"></i>
-    </div>
+    
         </section>
     );
 }
--- src/Component/Project/WorkItems.jsx
+++ src/Component/Project/WorkItems.jsx
@@ -1,5 +1,6 @@
 export default function WorkItems({ item }) {
     return (
+        
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className="work__img" />
             <h3 className="work__title">{item.title}</h3>
