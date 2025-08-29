d99976f216eb9d71b927ec2bd2299942efe3a0b8
Mesh Gawar <meshgawar@gmail.com>
2025-08-29 14:31:22 +0530

    Footer Added Need Changes and updation in Css

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,8 +2,51 @@ import "./Footer.css";
 
 export default function Footer() {
     return (
-        <section className="footer section" id="footer">
-    
+        <section className="footer">
+            <div className="footer__container container">
+                <h1 className="footer title">Mesh</h1>
+
+                <ul className="footer__list">
+                    <p style={{color:"red",marginBottom:"1rem"}}>Main</p>
+                    <li>
+                        <a href="index.html" className="footer__link">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="#about" className="footer__link">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#project" className="footer__link">Projects</a>
+                    </li>
+
+                    <li>
+                        <a href="#qualification" className="footer__link">Qualification</a>
+                    </li>
+                </ul>
+
+                <div className="footer__social">
+                    <a href="https://www.linkedin.com/in/meshgawar24/" className="footer__social-link" target="_blank">
+                        <i className="bx bxl-linkedin"></i>
+                        LinkedIn
+                    </a>
+
+                    <a href="https://github.com/meshgawar/" className="footer__social-link" target="_blank">
+                        <i className="uil uil-github-alt"></i>
+                        GitHub
+                    </a>
+                    <a href="https://www.youtube.com/" className="footer__social-link">
+                        <i className="bx bxl-youtube "></i>
+                        YouTube
+                    </a>
+                    <a href="https://leetcode.com/u/meshgawar24/" className="footer__social-link" target="_blank">
+                        <i className="bxl  bx-leetcode "></i>
+                        Leetcode
+                    </a>
+                </div>
+
+                <span className="footer__copy">&#169; Mesh Gawar. All rights reserved</span>
+            </div>
         </section>
     );
 }
--- src/Component/Home/Social.jsx
+++ src/Component/Home/Social.jsx
@@ -8,15 +8,15 @@ export default function Social() {
     // }
     return(
         <div className="home__social">
-            <a href="https://dribbble.com/" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/meshgawar24/" className="home__social-icon" target="_blank">
                 <i className="bx bxl-linkedin"></i>
             </a>
 
-            <a href="https://github.com/" className="home__social-icon" target="_blank">
+            <a href="https://github.com/meshgawar/" className="home__social-icon" target="_blank">
                 <i className="uil uil-github-alt"></i>
             </a>
 
-            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+            <a href="https://leetcode.com/u/meshgawar24/" className="home__social-icon" target="_blank">
                 <i className="bxl  bx-leetcode "></i>
             </a>
         
