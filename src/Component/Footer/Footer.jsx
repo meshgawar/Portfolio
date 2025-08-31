4ac6b0980b3b6586060deba62c3d99817cdd1a93
Mesh Gawar <meshgawar@gmail.com>
2025-08-31 11:50:36 +0530

    Footer Css updated

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,47 +4,75 @@ export default function Footer() {
     return (
         <section className="footer">
             <div className="footer__container container">
-                <h1 className="footer title">Mesh</h1>
+                {/* <h1 className="footer title">Mesh</h1> */}
+                <div className="footer__container-child">
+                    <div className="footer__list">
+                        <h4 className="list__title">Main</h4>
+                        <ul>
+                            <li>
+                                <a href="index.html" className="footer__link">Home</a>
+                            </li>
 
-                <ul className="footer__list">
-                    <p style={{color:"red",marginBottom:"1rem"}}>Main</p>
-                    <li>
-                        <a href="index.html" className="footer__link">Home</a>
-                    </li>
+                            <li>
+                                <a href="#project" className="footer__link">Projects</a>
+                            </li>
 
-                    <li>
-                        <a href="#about" className="footer__link">About</a>
-                    </li>
+                            <li>
+                                <a href="#contact" className="footer__link">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
 
-                    <li>
-                        <a href="#project" className="footer__link">Projects</a>
-                    </li>
+                    <div className="footer__list">
+                        <h4 className="list__title">About</h4>
+                        <ul>
+                            <li>
+                                <a href="#about" className="footer__link">About Me</a>
+                            </li>
 
-                    <li>
-                        <a href="#qualification" className="footer__link">Qualification</a>
-                    </li>
-                </ul>
+                            <li>
+                                <a href="#skills" className="footer__link">Skills</a>
+                            </li>
 
-                <div className="footer__social">
-                    <a href="https://www.linkedin.com/in/meshgawar24/" className="footer__social-link" target="_blank">
-                        <i className="bx bxl-linkedin"></i>
-                        LinkedIn
-                    </a>
+                            <li>
+                                <a href="#qualification" className="footer__link">Qualification</a>
+                            </li>
+                        </ul>
+                    </div>
 
-                    <a href="https://github.com/meshgawar/" className="footer__social-link" target="_blank">
-                        <i className="uil uil-github-alt"></i>
-                        GitHub
-                    </a>
-                    <a href="https://www.youtube.com/" className="footer__social-link">
-                        <i className="bx bxl-youtube "></i>
-                        YouTube
-                    </a>
-                    <a href="https://leetcode.com/u/meshgawar24/" className="footer__social-link" target="_blank">
-                        <i className="bxl  bx-leetcode "></i>
-                        Leetcode
-                    </a>
+                    <div className="footer__list">
+                        <h4 className="list__title">Contact</h4>
+                        <ul>
+                            <li>
+                                <a href="#contact" className="footer__link">Contact Me</a>
+                            </li>
+
+                            <li>
+                                <a href="#contact" className="footer__link">Message Me</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__social">
+                        <a href="https://www.linkedin.com/in/meshgawar24/" className="footer__social-link" target="_blank">
+                            <i className="bx bxl-linkedin"></i>
+                            LinkedIn
+                        </a>
+
+                        <a href="https://github.com/meshgawar/" className="footer__social-link" target="_blank">
+                            <i className="uil uil-github-alt"></i>
+                            GitHub
+                        </a>
+                        <a href="https://www.youtube.com/" className="footer__social-link">
+                            <i className="bx bxl-youtube "></i>
+                            YouTube
+                        </a>
+                        <a href="https://leetcode.com/u/meshgawar24/" className="footer__social-link" target="_blank">
+                            <i className="bxl  bx-leetcode "></i>
+                            Leetcode
+                        </a>
+                    </div>
                 </div>
-
                 <span className="footer__copy">&#169; Mesh Gawar. All rights reserved</span>
             </div>
         </section>
